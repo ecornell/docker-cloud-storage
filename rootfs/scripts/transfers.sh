40424896b71b4062f6d46a619710dde5ddec1ef0
T.C. Ferguson <tcf909@gmail.com>
2017-03-10 15:44:05 -0800

    .

---
+++ rootfs/scripts/transfers.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SOURCE_DIR="${1}" && shift
+DEST_DIR="${2}" && shift
+FILES=("${@}")
+
+#list recursive oldest first
+#find . -type f -exec ls -tr {} +
+
+#available mbs
+#df -m . | tail -1 | awk '{print $4}'
+
+#total mbs
+#df -m .  | tail -1 | awk '{print $2}'
+
+#percentage
+#percent=$((200*$item/$total % 2 + 100*$item/$total))
